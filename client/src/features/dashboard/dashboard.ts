13d1dd39f8b87d0212146571988abb0cf517d94a
Yue Yu <yueyu0214@gmail.com>
2025-12-01 12:24:20 -0500

    Added dashboard UI

--- client/src/features/dashboard/dashboard.ts
+++ client/src/features/dashboard/dashboard.ts
@@ -1,11 +1,19 @@
 import { Component } from '@angular/core';
+import { MATERIAL_IMPORTS } from '../../shared/material';
 
 @Component({
   selector: 'app-dashboard',
-  imports: [],
+  imports: [MATERIAL_IMPORTS],
   templateUrl: './dashboard.html',
   styleUrl: './dashboard.css',
 })
 export class Dashboard {
+  summaryMetrics = [
+    { label: 'Calories', unit: 'kcal' },
+    { label: 'Protein', unit: 'g' },
+    { label: 'Carbs', unit: 'g' },
+    { label: 'Fat', unit: 'g' }
+  ];
 
+  meals = ['Breakfast', 'Lunch', 'Dinner'];
 }
